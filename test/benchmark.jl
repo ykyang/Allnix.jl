3cc4490358cbac4218706c9bef89ad7f58d06f5d
Yi-Kun Yang <ykyang@gmail.com>
2017-08-30 10:43:26 -0500

    Check results in AXPY benchmark

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -1,6 +1,7 @@
 using Allnix: axpy
 using Allnix.Test: init!
 using BenchmarkTools
+using Base.Test
 
 
 # b = @benchmarkable Allnix.daxpy(0.5, xy[1], xy[2]) setup=(xy = init{Float64}())
@@ -26,13 +27,42 @@ function run_axpy{T}(n::Int64, α::T)
     x = Vector{T}(n)
     y = Vector{T}(n)
 
+    # - Calculate the answer - #
+    # - I want to double check the calculation is actually
+    # - done in the benchmark code.
+    init!(x, y)
+    ans_1 = α*x[1] + y[1]
+    ans_n = α*x[n] + y[n]
+
     b = @benchmarkable axpy($α, $x, $y) setup=(init!($x,$y))
     t = BenchmarkTools.run(b, samples = 3)
     show(STDOUT, "text/plain", t)
     print("\n\n")
-    # println(y[n])
+
+    @test ans_1 == y[1]
+    @test ans_n == y[n]
 end
 
+function axpy_ifelse(α::Float64, x::Vector{Float64}, y::Vector{Float64})
+    @inbounds for i in 1:length(x)
+        if x[i] < 0.5
+            y[i] = α*x[i] + y[i]
+        else
+            y[i] = α*x[i] - y[i]
+        end
+    end
+end
+
+function run_axpy_ifelse(n::Int64, α::Float64)
+    println("Testing axpy with if-else")
+    x = Vector{Float64}(n)
+    y = Vector{Float64}(n)
+
+    b = @benchmarkable axpy_ifelse($α, $x, $y) setup(init!($x, $y))
+    t = BenchmarkTools.run(b, samples = 3)
+    show(STDOUT, "text/plain", t)
+    print("\n\n")
+end
 
 function run()
     n::Int64 = 1000000
@@ -40,6 +70,8 @@ function run()
     run_axpy(n, Float32(0.5))
     run_axpy(n, 2)
     run_axpy(n, Int32(2))
+
+    run_axpy_ifelse(n, 0.5)
 end
 
 run()
